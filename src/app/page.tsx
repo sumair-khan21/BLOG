fbc6673ed173bceee8353588e29aaeb18fe9b17d
SUMAIR KHAN <117274549+sumair-khan21@users.noreply.github.com>
2025-02-02 21:05:15 +0500

    working

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "./components/hero/page";
+import Navbar from "./components/navbar/page";
+
+
+export default function Home() {
+  return (
+   <>
+  <Navbar />
+  <Hero />
+   </>
+  );
+}
